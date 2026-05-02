e24287097eb8c6f0279badf089ef566884fdeb82
k1r0nk321 <nakamae@mub.biglobe.ne.jp>
2026-05-02 16:34:50 +0900

    Add icons to metadata in layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,10 @@ import './globals.css'
 export const metadata = {
   title: 'ER Training',
   description: 'ER症例学習アプリ',
+  icons: {
+    apple: '/api/icon',
+    icon: '/api/icon',
+  },
 }
 
 export default function RootLayout({ children }) {
